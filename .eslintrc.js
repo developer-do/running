d45db78dd4bf099b2d51965f6fe14da7248aa937
developer-do <dev-do@naver.com>
2018-11-08 22:48:52 +0900

    really script gogogit add -A!

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    // "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "off",
+        "indent": [
+            2,
+            4
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
 
 let SRC = {
     es6: './src/es6/**/*.js',
     dist: './src/dist'
-}
+};
 
 let PUBLIC = {
     es6: './public/es6/**/*.js',
     dist: './public/dist'
-}
+};
+
 
 gulp.task("default", function () {
+    gulp.src([SRC.es6, PUBLIC.es6])
+        .pipe(eslint())
+        .pipe(eslint.format());
     gulp.src(SRC.es6)
         .pipe(babel())
         .pipe(gulp.dest(SRC.dist));
