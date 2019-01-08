29ccae5ddfea1cc6e2ca66e1fe4b8461865ad284
dev-do <dev-do@naver.com>
2019-01-08 13:54:45 +0900

    step12 completegit add -Agit add -A

---
+++ public/es6/step12/12.2.2 제너레이터와 return/main.js
@@ -0,0 +1,15 @@
+function* abc() {
+  yield 'a';
+  yield 'b';
+  return 'c';
+}
+
+const it = abc();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+for (let l of abc()) {
+  console.log(l);
+}
