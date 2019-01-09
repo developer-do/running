58384896fae968727356444c224aae72620d6b0f
developer-do <dev-do@naver.com>
2019-01-10 00:59:56 +0900

    step16 Math객체 부분은 필요할 때 찾아보기.

---
+++ public/es6/step16/16.1 숫자 형식/main.js
@@ -0,0 +1,42 @@
+const x = 19.51;
+console.log(x.toFixed(3));
+console.log(x.toFixed(2));
+console.log(x.toFixed(1));
+console.log(x.toFixed(0));
+
+console.log("\n");
+
+const xx = 3800.5;
+console.log(xx.toExponential(4));
+console.log(xx.toExponential(3));
+console.log(xx.toExponential(2));
+console.log(xx.toExponential(1));
+console.log(xx.toExponential(0));
+
+console.log("\n");
+
+let x1 = 1000;
+console.log(x1.toPrecision(5));
+console.log(x1.toPrecision(4));
+console.log(x1.toPrecision(3));
+console.log(x1.toPrecision(2));
+console.log(x1.toPrecision(1));
+
+console.log("\n");
+
+x1 = 15.335;
+console.log(x1.toPrecision(6));
+console.log(x1.toPrecision(5));
+console.log(x1.toPrecision(4));
+console.log(x1.toPrecision(3));
+console.log(x1.toPrecision(2));
+console.log(x1.toPrecision(1));
+
+console.log("\n");
+
+const x2 = 12;
+console.log(x2.toString());
+console.log(x2.toString(10));
+console.log(x2.toString(16));
+console.log(x2.toString(8));
+console.log(x2.toString(2));
