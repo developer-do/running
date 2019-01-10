4aa523bc361877de0281b1cd8b65523a8cd8d895
developer-do <dev-do@naver.com>
2019-01-11 01:27:30 +0900

    running -> running book change

---
+++ public/es6/step18/18.4 get 메서드/main.js
@@ -0,0 +1,8 @@
+const content = document.getElementById("content");
+console.log(content);
+const callouts = document.getElementsByClassName("callout");
+console.log(callouts);
+const callout = document.querySelector(".callout");
+console.log(callout);
+const pTag = [...document.getElementsByTagName("p")];
+console.log(pTag);
